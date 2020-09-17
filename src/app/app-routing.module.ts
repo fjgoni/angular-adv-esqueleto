663a1692350c3811ee3ac1b1f65f27cb0909e8f7
fjgoni <f.goni@usal.edu.ar>
2020-09-17 17:47:02 -0300

    fin seccion 3 - diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { E404Component } from './pages/e404/e404.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PagesComponent,
+  children: [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'progress', component: ProgressComponent},
+    {path: 'grafica1', component: Grafica1Component},
+    {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+  ]
+  },
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+
+  
+  {path: '**', component: E404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/e404/e404.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-e404',
+  templateUrl: './e404.component.html',
+  styleUrls: ['./e404.component.css']
+})
+export class E404Component{
+  year : number = new Date().getFullYear();
+  constructor() { 
+   
+  }
+
+  
+
+}
